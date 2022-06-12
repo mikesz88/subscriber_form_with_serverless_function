99b560f49efebc49db19513e0a1c16e3546df2a5
michael <michaelsanchez8815@gmail.com>
2022-06-12 00:26:29 -0500

    update antdForm. validation sub complt. onSub next

--- src/Features/Subscribe/index.jsx
+++ src/Features/Subscribe/index.jsx
@@ -1,22 +1,25 @@
+/* eslint-disable no-useless-escape */
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { StyledButton } from '../../Components/StyledButton';
 import { StyledHeader } from '../../Components/StyledHeader';
 import { UserAddOutlined} from '@ant-design/icons'
+import { Form, Input } from "antd";
 
 const Subscribe = () => {
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phone, setPhone] = useState("");
+  const [form] = Form.useForm();
+  const [isDisabled, setIsDisabled] = useState(true)
 
-  const resetForm = () => {
-    setEmail("");
-    setFirstName("");
-    setLastName("");
-    setPhone("");
-  };
+  const onFieldsChange = (_, field) => {
+    console.log(field);
+    if ((field.every(object =>object.errors.length === 0)) 
+    && field.every(object => !!object.value)) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }
 
   const onSubmit = async e => {
     e.preventDefault();
@@ -24,14 +27,14 @@ const Subscribe = () => {
       "Content-Type": "application/json"
     };
     const body = {
-      email,
-      firstName,
-      lastName,
-      phone
+      // email,
+      // firstName,
+      // lastName,
+      // phone
     };
     try {
       await axios.post("/api/subscribe", body, { headers });
-      resetForm();
+      // resetForm();
     } catch (error) {
       console.error(error);
     }
@@ -44,62 +47,92 @@ const Subscribe = () => {
         Subscribe
       <UserAddOutlined />
       </StyledHeader>
-      <form onSubmit={onSubmit}>
-        <div>
-          <input
-            type="text"
-            id="email"
-            required
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <label htmlFor="email">
-            Email Address
-          </label>
-        </div>
-        <div>
-          <input
-            type="text"
-            id="firstName"
-            required
-            value={firstName}
-            onChange={e => setFirstName(e.target.value)}
-          />
-          <label htmlFor="firstName">
-            First Name
-          </label>
-        </div>
-        <div>
-          <input
-            type="text"
-            id="lastName"
-            required
-            value={lastName}
-            onChange={e => setLastName(e.target.value)}
-          />
-          <label htmlFor="lastName">
-            Last Name
-          </label>
-        </div>
-        <div>
-          <input
-            type="tel"
-            id="phone"
-            required
-            value={phone}
-            onChange={e => setPhone(e.target.value)}
-          />
-          <label htmlFor="phone">
-            Phone
-          </label>
-        </div>
-        <div>
-          <StyledButton block type="submit">Subscribe</StyledButton>
-        </div>
-      </form>
+      <Form
+        name="subscribe"
+        form={form}
+        onFinish={onSubmit}
+        onFieldsChange={onFieldsChange}
+        autoComplete="off"
+        labelAlign="left"
+        labelCol={{
+          span: 10,
+        }}
+        wrapperCol={{
+          span: 20,
+        }}
+        size="large"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              type: 'email', 
+              message: 'Please write a valid email'
+            },
+            {
+              required: true,
+              message: 'Please write your email'
+            }
+          ]}
+        >
+          <Input type="email" required />
+        </Form.Item>
+        <Form.Item
+          label="First Name"
+          name="firstName"
+          rules={[
+            {
+              pattern: /^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/,
+              message: 'This is not a proper name'
+            },
+            {
+              required: true,
+              message: 'Please write your first name'
+            }
+          ]}
+        >
+          <Input type="text" required />
+        </Form.Item>
+        <Form.Item
+          label="Last Name"
+          name="lastName"
+          rules={[
+            {
+              pattern: /^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/,
+              message: 'This is not a proper name'
+            },
+            {
+              required: true,
+              message: 'Please write your Last name'
+            }
+          ]}
+        >
+          <Input type="text" required />
+        </Form.Item>
+        <Form.Item
+          label="Phone Number"
+          name="phone"
+          rules={[
+            {
+              pattern: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
+              message: 'Please write a proper phone number'
+            },
+            {
+              required: true,
+              message: 'Please write your phone number'
+            }
+          ]}
+        >
+          <Input type="phone" required />
+        </Form.Item>
+        <Form.Item noStyle>
+          <StyledButton disabled={isDisabled} block htmlType="submit">Subscribe</StyledButton>
+        </Form.Item>
+      </Form>
       <nav>
-        <Link onClick={resetForm} to="/findSubscriber">Find Subscriber</Link> |{" "}
-        <Link onClick={resetForm} to="/unsubscribe">Unsubscribe</Link>
+        <Link to="/findSubscriber">Find Subscriber</Link> |{" "}
+        <Link  to="/unsubscribe">Unsubscribe</Link>
       </nav>
     </div>
   );
